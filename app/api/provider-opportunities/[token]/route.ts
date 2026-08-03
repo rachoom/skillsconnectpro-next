747748992ffe86926dbcabf32a8eda75f2af0586
rachoom <rachoom77@gmail.com>
2026-08-04 01:20:14 +0200

    Accept planned provider availability responses

--- app/api/provider-opportunities/[token]/route.ts
+++ app/api/provider-opportunities/[token]/route.ts
@@ -13,6 +13,8 @@ const RESPONSE_TYPES: ProviderResponseType[] = [
   'available_now',
   'available_today',
   'available_tomorrow',
+  'available_this_week',
+  'available_next_week',
   'site_visit',
   'estimate',
   'need_information',
